a893af54a0f8a8f7d35a73e1a2e1e4e5b9d834e5
Brandon Hang <brandonshang@gmail.com>
2016-10-08 02:54:29 -0400

    Updated input sliders to be more user-friendly; more closely resembles mockup

--- hipec/static/js/hipec-index.js
+++ hipec/static/js/hipec-index.js
@@ -1,18 +1,44 @@
 (function($) {
-	$.each($('input[type=range]'), function(index, input) {			// Displays initial values of input sliders
-		var value = $(input).val();
-		var name = $(input).attr('name');
-		console.log(name);
-		$('#range-' + name).text(value);
+	$.each($('input[type=range]'), function(index, input) {
+		// noUiSlider.js prep work
+		var name = $(this).attr('name');
+		var min = $(this).attr('min');
+		var max = $(this).attr('max');
+		min = parseInt(min, 10);
+		max = parseInt(max, 10);
+		var mid = Math.round((min + max) / 2);
+		
+		// Creates better sliders via noUiSlider.js plugin
+		var slider = document.getElementById('nouisl-' + name);
+		slider.style.width = '85%';
+		noUiSlider.create(slider, {
+			start: [mid],
+			step: 1,
+			range: {
+				'min': min,
+				'max': max
+			},
+			pips: {
+				mode: 'count',
+				values: 10,
+				density: 3
+			}
+		});
 	});
 	
-	$('.category-title').on('click', function() {			// Opens and closes variable categories
+	// Opens and closes variable categories
+	$('.category-title').on('click', function() {
 		$(this).toggleClass('collapsed');
 	});
 	
-	$('input[type=range]').on('change', function() {		// Updates the input slider values
-		var value = $(this).val();
-		var name = $(this).attr('name');
-		$('#range-' + name).text(value);
+	// Updates the input slider values; I don't think Django can access
+	// the noUiSlider values directly as it is not a form input.
+	$.each($('.slider'), function(index, slider) {
+		slider.noUiSlider.on('update', function() {
+			name = $(slider).attr('slide');
+			value = Math.round(slider.noUiSlider.get());
+			$('input[slide="' + name + '"]').val(value);
+			$('#range-' + name).text(value);
+		});
 	});
 })(jQuery);
