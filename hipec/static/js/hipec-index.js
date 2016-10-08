0147d65f2bafd57df4c7ac5ee166fa6a3d053912
Brandon Hang <brandonshang@gmail.com>
2016-10-08 13:49:33 -0400

    Reworked hidden range inputs to number inputs

--- hipec/static/js/hipec-index.js
+++ hipec/static/js/hipec-index.js
@@ -1,15 +1,18 @@
 (function($) {
-	$.each($('input[type=range]'), function(index, input) {
+	$.each($('input[slide=true]'), function(index, input) {
 		// noUiSlider.js prep work
 		var name = $(this).attr('name');
 		var min = $(this).attr('min');
 		var max = $(this).attr('max');
+		var pips = $(this).attr('pips');
 		min = parseInt(min, 10);
 		max = parseInt(max, 10);
+		pips = parseInt(pips, 10);
 		var mid = Math.round((min + max) / 2);
+		density = Math.floor(10 - ((max - min) / pips));
 		
 		// Creates better sliders via noUiSlider.js plugin
-		var slider = document.getElementById('nouisl-' + name);
+		var slider = document.getElementById('slide_' + name);
 		slider.style.width = '85%';
 		noUiSlider.create(slider, {
 			start: [mid],
@@ -20,8 +23,9 @@
 			},
 			pips: {
 				mode: 'count',
-				values: 10,
-				density: 3
+				values: pips,
+				density: density,
+				stepped: true
 			}
 		});
 	});
@@ -31,14 +35,22 @@
 		$(this).toggleClass('collapsed');
 	});
 	
-	// Updates the input slider values; I don't think Django can access
-	// the noUiSlider values directly as it is not a form input.
+	// Updates the form input values; I don't think Django can access
+	// the noUiSlider values directly as it is not an HTML form input.
 	$.each($('.slider'), function(index, slider) {
 		slider.noUiSlider.on('update', function() {
 			name = $(slider).attr('slide');
 			value = Math.round(slider.noUiSlider.get());
-			$('input[slide="' + name + '"]').val(value);
+			$('input[name="' + name + '"]').val(value);
 			$('#range-' + name).text(value);
 		});
+	});
+	
+	// Updates the sliders when a value is entered directly into the input box
+	$('input[slide="true"]').on('change', function() {
+		var value = $(this).val();
+		var name = $(this).attr('name');
+		var slider = document.getElementById('slide_' + name);
+		slider.noUiSlider.set(value);
 	});
 })(jQuery);
