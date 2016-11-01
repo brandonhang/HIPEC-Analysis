d4f098017962fd5fa8183761dc60366ebd7e76b0
Brandon Hang <brandonshang@gmail.com>
2016-11-01 17:31:27 -0400

    Added automatic filtering of core variables from cleaned data.

--- hipec/models.py
+++ hipec/models.py
@@ -7,12 +7,10 @@ import pandas, numpy
 import pickle
 
 class SurvivalAnalysis(models.Model):
-	""" This function doesn't do anything meaningful outside of adding today's
-	date. It serves as a placeholder for when an actual model is implemented
-	to calculate results based on input.
-	"""
+	''' This function filters out the HIPEC Excel data for relevant patient
+	information.  It is then used to generate the output page. '''
 	@staticmethod
-	def dummy_function(captured_data):
+	def filter_data(captured_data):
 		''' This function reads in a Yes/No response and returns True/False.
 		It returns None if the response is not 'Yes' or 'No'. '''
 		def parse_boolean(boolean):
@@ -70,6 +68,10 @@ class SurvivalAnalysis(models.Model):
 					pci = row[' Intra Op PCI Score ']
 					cc_score = row['Post-Op-CC-Score']
 					
+					# Additional data for survival calculations
+					s_time = row['Survival time from Surgery']
+					vitals = row['Vital Status at analysis/SSDI']
+					
 					# Attempt to enforce data types
 					try:
 						age = float(age)
@@ -118,6 +120,16 @@ class SurvivalAnalysis(models.Model):
 					if 'No Data' in disposition or disposition == 'nan':
 						disposition = None
 					
+					# Clean survival calculation data
+					try:
+						survival_t = float(s_time)
+					except ValueError:
+						survival_t = None
+					
+					vital_stat = str(vitals)
+					
+					if 'No Data' in vital_stat or vital_stat == 'nan':
+						vital_stat = None
 					
 					# Morbidity and mortality -> booleans
 					morb_30 = row['Morbidity < 30Days']
@@ -157,7 +169,9 @@ class SurvivalAnalysis(models.Model):
 						'Morbidity90': morbidity_90,
 						'Mortality30': mortality_30,
 						'Mortality60': mortality_60,
-						'Mortality90': mortality_90
+						'Mortality90': mortality_90,
+						'SurvivalTime': survival_t,
+						'VitalStatus': vital_stat
 					}
 			
 			return hipec_data
@@ -167,24 +181,11 @@ class SurvivalAnalysis(models.Model):
 		def get_all_hipec_data_of(variable, master_data):
 			data_list = []
 			
-			for p_id, patient in master_data.items():
-				for op_id, operation in patient['HIPEC'].items():
-					if variable in operation:
-						if operation[variable] is not None:
-							data_list.append(operation[variable])
+			for id_op_key, patient in master_data.items():
+				data_list.append(patient[variable])
 			
-			sorted(data_list)
 			return data_list
 		''' End get_all_hipec_data_of() '''
-		
-		''' Test function to return hospital length of stay '''
-		def sample_hospital_length_of_stay(data_list):
-			q1 = numpy.percentile(data_list, 25)
-			median = numpy.percentile(data_list, 50)
-			q3 = numpy.percentile(data_list, 75)
-			
-			return {'Median': median, 'Q1': q1, 'Q3': q3}
-		''' End sample_hospital_length_of_stay() '''
 		
 		def sample_disposition_status(data_list):
 			dispo = {}
@@ -203,13 +204,43 @@ class SurvivalAnalysis(models.Model):
 			answer = {}
 			
 			for boolean in data_list:
-				if boolean not in answer:
-					answer[boolean] = 1
+				bool_key = ''
+				
+				if boolean == True:
+					bool_key = 'Yes'
+				elif boolean == False:
+					bool_key = 'No'
 				else:
-					answer[boolean] += 1
+					bool_key = 'NoData'
+				
+				if bool_key not in answer:
+					answer[bool_key] = 1
+				else:
+					answer[bool_key] += 1
 			
 			return answer
 		''' End sample_yes_no_answer() '''
+		
+		def filter_patient_data(data):
+			filtered = {}
+			
+			for p_id, patient in data.items():
+				# Filtering of demographic info would go here
+				# 'continue' if demographics don't match
+				for op_num, op_data in patient['HIPEC'].items():
+					if op_data['PrimaryTumor'] == captured_data['core_primary_tumor'] \
+						and op_data['Age'] >= captured_data['core_age__lower'] \
+						and op_data['Age'] <= captured_data['core_age__upper'] \
+						and op_data['CCI'] >= captured_data['core_charlson__lower'] \
+						and op_data['CCI'] <= captured_data['core_charlson__upper'] \
+						and op_data['KPS'] == captured_data['core_karnofsky'] \
+						and op_data['PCI'] >= captured_data['core_peritoneal__lower'] \
+						and op_data['PCI'] <= captured_data['core_peritoneal__upper'] \
+						and op_data['CC'] == captured_data['core_cytoreduction']:
+						
+						filtered[(p_id, op_num)] = op_data
+			
+			return filtered
 		
 		modded = int(os.path.getmtime('hipec\\data\\HIPEC_data.xlsx'))
 		modded = str(modded)
@@ -224,19 +255,23 @@ class SurvivalAnalysis(models.Model):
 			with open(pickle_path, 'wb') as handle:
 				pickle.dump(hipec_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 		
+		filtered_data = filter_patient_data(hipec_data)
+		
+		if len(filtered_data) == 0:
+			return captured_data
+		
 		today = datetime.date.today()
-		hospital_hist = get_all_hipec_data_of('HospitalStay', hipec_data)
-		hospital = sample_hospital_length_of_stay(hospital_hist)
-		disposition = get_all_hipec_data_of('Disposition', hipec_data)
+		hospital = get_all_hipec_data_of('HospitalStay', filtered_data)
+		disposition = get_all_hipec_data_of('Disposition', filtered_data)
 		disposition = sample_disposition_status(disposition)
-		readmission = get_all_hipec_data_of('Readmission', hipec_data)
+		readmission = get_all_hipec_data_of('Readmission', filtered_data)
 		readmission = sample_yes_no_answer(readmission)
-		morbidity = get_all_hipec_data_of('Morbidity90', hipec_data)
+		morbidity = get_all_hipec_data_of('Morbidity90', filtered_data)
 		morbidity = sample_yes_no_answer(morbidity)
-		mortality = get_all_hipec_data_of('Mortality90', hipec_data)
+		mortality = get_all_hipec_data_of('Mortality90', filtered_data)
 		mortality = sample_yes_no_answer(mortality)
 		captured_data.update({'today': today})
-		captured_data.update({'HospitalHistogram': hospital_hist})
+		captured_data.update({'HospitalHistogram': hospital})
 		captured_data.update({'HospitalRange': hospital})
 		captured_data.update({'Disposition': disposition})
 		captured_data.update({'Readmission': readmission})
