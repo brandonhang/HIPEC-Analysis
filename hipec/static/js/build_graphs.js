d6383769604cecbf35246fb0e26bc61a8b2ded8d
Brandon Hang <brandonshang@gmail.com>
2016-11-01 17:32:45 -0400

    Cleaned up histogram generation.

--- hipec/static/js/build_graphs.js
+++ hipec/static/js/build_graphs.js
@@ -9,10 +9,21 @@ function buildGraphs(jsonStr) {
 	jsonStr = jsonStr.replace(/None/g, 'null');
 	jsonStr = jsonStr.replace(/datetime\.date\((\d*?), (\d*?), (\d*?)\)/g,
 		"[$1, $2, $3]");
-	jasonBourne = $.parseJSON(jsonStr);console.log(jasonBourne);
+	jasonBourne = $.parseJSON(jsonStr);
 	
 	google.charts.load('current', {'packages': ['corechart']});
 	google.charts.setOnLoadCallback(buildHospitalHistogram);
+	
+	// Fake median and IQR
+	var phony = [];
+	
+	for (var i = 0; i < 3; i++) {
+		phony.push(Math.floor(Math.random() * 50) + 1);
+	}
+	
+	phony.sort(function(a, b) { return a - b; });
+	$('#median').text(phony[1] + (phony[1] == 1 ? ' day' : ' days'));
+	$('#iqr').html(phony[0] + '&ndash;' + phony[2]);
 	
 	function buildHospitalHistogram() {
 		// To build a histogram, Google Charts wants every value to be assigned
